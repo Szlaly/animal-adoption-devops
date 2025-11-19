44ee4687c409a8fd567a603903bfc4890a354d02
Balint Tamas <lordtomi18@gmail.com>
2025-11-19 20:03:53 +0100

    Promethus build fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,9 +7,10 @@ import userRoutes from "./routes/user.routes";
 import adoptionRoutes from "./routes/adoption.routes";
 import supportRoutes from './routes/support.routes';
 import path from 'path';
+import client from "prom-client";
 
 const app = express();
-
+client.collectDefaultMetrics();
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -19,6 +20,12 @@ app.use("/api/users", userRoutes);
 app.use('/api/support', supportRoutes);
 app.use("/api/adoptions", adoptionRoutes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+// Prometheus 
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 // teszt route
 app.get("/", (req, res) => {
